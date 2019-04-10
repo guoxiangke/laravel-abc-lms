55b119089fb0809d7ef0aa5378a42156dd21d32b
dale@mac <xiangkeguo@gmail.com>
2019-04-10 18:43:00 +0800

    up

--- resources/views/classRecords/index.blade.php
+++ resources/views/classRecords/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="container">
-  <h1>{{__('ClassRecords')}} {{isset($roleName)? ' - ' . $userName : ''}}</h1>
+  <h1>{{__('ClassRecords')}}</h1>
   
   <div class="show-links">
       <a href="{{ route('home') }}" class="btn btn-outline-dark"><i class="fas fa-angle-left fa-large"></i> Go Back</a>
@@ -21,51 +21,19 @@
                 <th scope="col">Agency</th>
                 <th scope="col">ClassAt</th>
               	<th scope="col">exception</th>
-                @if(!isset($roleName))
-                <th scope="col">order_id</th>
-                @endif
               </tr>
             </thead>
             <tbody>
               @foreach($classRecords as $classRecord)
                   <tr>
                     <th scope="row">
-                      
-                      <a class="btn btn-sm btn-info text-uppercase" href="{{ route('classRecords.show', $classRecord->id) }}">{{$classRecord->id}}</a>
-                      @if(isset($roleName))
-                          @if($roleName === 'teacher')
-                            @if(!$classRecord->remark)
-                              <a class="btn btn-sm btn-warning text-uppercase" href="{{ route('classRecords.edit', $classRecord->id) }}">!Evaluation</a>
-                              <a class="btn btn-sm btn-primary text-uppercase" href="https://zoom.us/j/{{ $classRecord->teacherModel->zoom->pmi }}">Zoom</a>
-                            @endif
-
-                            @if(!$mp3Url = $classRecord->getFirstMediaUrl('mp3'))
-                            <a class="btn btn-sm btn-warning text-uppercase" href="{{ route('classRecords.edit', $classRecord->id) }}">!Mp3</a>
-                            @endif
-
-                            @if(!$mp4Url = $classRecord->getFirstMediaUrl('mp4'))
-                            <a class="btn btn-sm btn-warning text-uppercase" href="{{ route('classRecords.edit', $classRecord->id) }}">!Mp4</a>
-                            @endif
-
-                          @endif
-                          
-                        @if($roleName === 'student')
-                          @if($classRecord->remark)
-                            <a class="btn btn-sm btn-warning text-uppercase" href="{{ route('classRecords.show', $classRecord->id) }}">课程回顾</a>
-                          @else
-                          <a class="btn btn-sm btn-primary text-uppercase" href="https://zoom.us/j/{{ $classRecord->teacherModel->zoom->pmi }}">加入课堂</a>
-                          @endif
-                        @elseif($roleName === 'agency')
-                         <a class="btn btn-sm btn-warning text-uppercase" href="{{ route('classRecords.show', $classRecord->id) }}">查看课程</a>
-                        @endif
-                      @else
+                      {{$classRecord->id}}
                       <a class="btn btn-sm btn-outline-dark text-uppercase" href="{{ route('classRecords.show', $classRecord->id) }}">
                         View
                       </a>
                       <a class="btn btn-sm btn-outline-dark text-uppercase" href="{{ route('classRecords.edit', $classRecord->id) }}">
                         Edit
                       </a>
-                      @endif
                     </th>
                     <td data-label="Teacher">{{$classRecord->user->profiles->first()->name}}</td>
                     <td data-label="student">{{$classRecord->teacher->profiles->first()->name}}</td>
@@ -73,13 +41,6 @@
                     <td data-label="ClassAt">{{$classRecord->generated_at->format('n/j H:i 周N')}}</td>
                     <td data-label="exception">{{\App\Models\ClassRecord::EXCEPTION_TYPES[$classRecord->exception]}}
                     </td>
-                    @if(!isset($roleName))
-                    <td data-label="Order">
-                      <a href="{{ route('classRecords.show',$classRecord->id) }}">
-                        {{$classRecord->rrule->order->title}}
-                      </a>
-                    </td>
-                    @endif
                   </tr>
               @endforeach
             </tbody>
