a337b13db24245dd66e5daa8a1ac03193f0ae431
dale@mac <xiangkeguo@gmail.com>
2019-04-10 16:36:23 +0800

    back swich

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
                                 </div>
                             </li>
                         @endguest
-                        @hasanyrole('manager|admin|teacher|student|agency')
+                        @hasanyrole('manager|admin')
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     Switch <span class="caret"></span>
