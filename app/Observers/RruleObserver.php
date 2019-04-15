2e899c77290301514b52585c2d81eff4b0bd8548
dale@mac <xiangkeguo@gmail.com>
2019-04-15 09:42:21 +0800

    no one month classrecords history

--- app/Observers/RruleObserver.php
+++ app/Observers/RruleObserver.php
@@ -16,12 +16,13 @@ class RruleObserver
     public function created(Rrule $rrule)
     {
         // 针对上课计划 而不是请假计划
-        if($rrule->type == Rrule::TYPE_SCHEDULE){
-            //自动生成过去1个月的记录，特殊记录请编辑修改状态！！！
-            for ($i=0; $i < 31; $i++) {
-                ClassRecordsGenerateQueue::dispatch($rrule->order,$i)->onQueue('high');
-            }
-            // ClassRecordsGenerateQueue::dispatch($rrule->order)->onQueue('high');
+        if($rrule->type == Rrule::TYPE_SCHEDULE)
+        {
+            ClassRecordsGenerateQueue::dispatch($rrule->order)->onQueue('high');
+            // //自动生成过去1个月的记录，特殊记录请编辑修改状态！！！
+            // for ($i=0; $i < 31; $i++) {
+            //     ClassRecordsGenerateQueue::dispatch($rrule->order,$i)->onQueue('high');
+            // }
         }
         
     }
