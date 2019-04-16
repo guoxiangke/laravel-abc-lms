b92942313f9053c1b3949056be6abc26d4814805
dale@mac <xiangkeguo@gmail.com>
2019-04-16 15:42:47 +0800

    bug delete

--- app/Services/MediaLibraryCustomPath.php
+++ app/Services/MediaLibraryCustomPath.php
@@ -13,7 +13,7 @@ class MediaLibraryCustomPath implements PathGenerator
     protected function getBasePath(Media $media): string
     {
         // return $media->getKey();
-        return 'media/'. $media->updated_at->format('Y/m/d');
+        return 'media/'. $media->updated_at->format('Y/m/d/') . $media->getKey();
     }
 
     /*
