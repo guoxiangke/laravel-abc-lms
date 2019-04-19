49f5abbef90a70b7de315822016e8b6f7d65ab20
dale@mac <xiangkeguo@gmail.com>
2019-04-19 12:27:22 +0800

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,9 +100,14 @@
 
                     @hasanyrole('manager|admin|agency')
                         <div class="col-md-4 pt-5  text-center bg-light">
-                            <h5 class="display-5">统一收款码</h5>
+                            <h5 class="display-5">支付宝统一收款码</h5>
                             <p class="lead">支付请备注学生姓名</p>
                             <img src="{{ asset('images/alipay.png')}}" alt="" width="300px" loading="lazy">
+                        </div>
+                        <div class="col-md-4 pt-5  text-center bg-light">
+                            <h5 class="display-5">微信统一收款码</h5>
+                            <p class="lead">支付请备注学生姓名</p>
+                            <img src="{{ asset('images/wechat.png')}}" alt="" width="300px" loading="lazy">
                         </div>
                     @endhasanyrole
                     </div>
