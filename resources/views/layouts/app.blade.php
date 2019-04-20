d6064299843b15c13cb87716a180c1816b30aee7
dale@mac <xiangkeguo@gmail.com>
2019-04-20 22:44:00 +0800

    session alertDetail

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,6 +115,9 @@
                 <div class="row alert alert-{{ $msg }} close-it">
                         <div class="col-sm-11  col-md-11 col-lg-11 col-xl-11 col-11">
                             {{ Session::get('alert-' . $msg) }}
+                            @if(session('alertDetail'))
+                                <pre class="alert-pre border bg-light p-2"><code>{{ session('alertDetail') }}</code></pre>
+                            @endif
                         </div>
                         <div class="col-sm-1  col-md-1 col-lg-1 col-xl-1 col-1">
                             <button type="button" class="close" aria-label="Close">
