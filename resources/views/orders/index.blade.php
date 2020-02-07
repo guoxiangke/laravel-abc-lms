60b8d51a775e9f7e5ee7ce88392ca34363cd548c
guoxiangke <xiangkeguo@gmail.com>
2020-02-07 18:26:37 +0800

    bug of index search

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -143,4 +143,36 @@
 })(jQuery);
 </script>
 @endsection
-@include('scripts.search')
+@section('scripts1')
+<script type="text/javascript">
+(function($) {
+  $( document ).ready(function() {
+
+    $('#reset').click(function(e){
+    e.preventDefault();
+    return window.location = window.location.pathname;
+    });
+
+    $('#studentName').keypress(function (e) {
+    if (e.which == 13) {
+      $('#search').trigger('click');
+      return false;    //<---- Add this line
+    }
+    });
+
+
+    $('#search').click(function(e){
+    e.preventDefault();
+    var regex = /[a-z]{2}/gi; //pinyin
+    $input = $('#studentName').val();
+
+    if(regex.test($input)){
+      return window.location = window.location.pathname + "?filter[student.name]="+$input;
+    }else{
+      return window.location = window.location.pathname + "?filter[student.profiles.name]="+$input;
+    }
+    });
+  });
+})(jQuery);
+</script>
+@endsection
